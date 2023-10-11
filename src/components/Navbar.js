895ff433538eff54e06eedfaaad1b8c4b2332745
arturxdking <arturxdking@gmail.com>
2023-10-11 18:29:59 -0300

    Estudando biblioteca react-router-dom

---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+// 2 - links com react router
+import './Navbar.css'
+
+import { Link, NavLink } from 'react-router-dom'
+
+const Navbar = () => {
+    return (
+        <nav>
+            {/* <Link to="/">Home</Link>
+            <Link to="/about">About</Link> */}
+            <NavLink
+                to="/"
+            // </nav>className={({isActive})=>(isActive ? "esta-ativo" : "nao-ativo")}
+            >Home
+            </NavLink>
+            <NavLink to="/about">About</NavLink>
+        </nav>
+    )
+}
+
+export default Navbar
